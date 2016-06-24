acae0a439cbdb8753abf5eb15ef9ad4e014f353c
Robert Chang <cht8687@gmail.com>
2016-06-24 23:48:34 +1000

    Move options to separate file.

--- src/example/Example.js
+++ src/example/Example.js
@@ -1,66 +1,7 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import Tooltip from '..'
-
-const optionsRight = {
-  direction: 'Right',
-  followCursor: false,
-  shiftX: 20,
-  shiftY: 0
-}
-
-const optionsTop = {
-  direction: 'Top',
-  followCursor: false,
-  shiftX: 20,
-  shiftY: 0
-}
-
-const tooltip1 = {
-  hoverComponent: 'I am the awesome tooltip 1!',
-  trigger: 'awesome tooltip1'
-};
-
-const tooltip2 = {
-  hoverComponent: 'I am the awesome tooltip 2!',
-  trigger: 'awesome tooltip2'
-};  
-
-const tooltip1styles = {
-  trigger: {
-    background: 'rgb(142, 147, 247)',
-    width: '200px',
-    marginLeft: '5px'
-  },
-
-  hoverComponent: {
-    height: '25px',
-    overflowY: 'auto',
-    width: '300px',
-    background: '#cccccc',
-    display: 'none',
-    position: 'absolute'
-  }
-};
-
-const tooltip2styles = {
-  trigger: {
-    background: 'rgb(58, 98, 247)',
-    width: '250px',
-    marginLeft: '5px'
-  },
-
-  hoverComponent: {
-    height: '25px',
-    overflowY: 'auto',
-    width: '250px',
-    borderRadius: '15px',
-    background: 'rgb(216, 226, 99)',
-    display: 'none',
-    position: 'absolute',
-    padding: '5px'
-  }
-};
+import { optionsRight, optionsTop, tooltip1styles, tooltip2styles } from './Options'
 
 class App extends Component {
   render() {
---
+++ src/example/Options.js
@@ -0,0 +1,59 @@
+export const optionsRight = {
+  direction: 'Right',
+  followCursor: false,
+  shiftX: 20,
+  shiftY: 0
+}
+
+export const optionsTop = {
+  direction: 'Top',
+  followCursor: false,
+  shiftX: 20,
+  shiftY: 0
+}
+
+export const tooltip1 = {
+  hoverComponent: 'I am the awesome tooltip 1!',
+  trigger: 'awesome tooltip1'
+};
+
+export const tooltip2 = {
+  hoverComponent: 'I am the awesome tooltip 2!',
+  trigger: 'awesome tooltip2'
+};  
+
+export const tooltip1styles = {
+  trigger: {
+    background: 'rgb(142, 147, 247)',
+    width: '200px',
+    marginLeft: '5px'
+  },
+
+  hoverComponent: {
+    height: '25px',
+    overflowY: 'auto',
+    width: '300px',
+    background: '#cccccc',
+    display: 'none',
+    position: 'absolute'
+  }
+};
+
+export const tooltip2styles = {
+  trigger: {
+    background: 'rgb(58, 98, 247)',
+    width: '250px',
+    marginLeft: '5px'
+  },
+
+  hoverComponent: {
+    height: '25px',
+    overflowY: 'auto',
+    width: '250px',
+    borderRadius: '15px',
+    background: 'rgb(216, 226, 99)',
+    display: 'none',
+    position: 'absolute',
+    padding: '5px'
+  }
+};
