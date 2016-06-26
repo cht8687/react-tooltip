440c46157118d241beb4b8a92d43bb5d65e85de5
Robert Chang <cht8687@gmail.com>
2016-06-26 17:40:44 +1000

    remove unused variables

--- src/example/Options.js
+++ src/example/Options.js
@@ -15,12 +15,7 @@ export const optionsTop = {
 export const tooltip1 = {
   hoverComponent: 'I am the awesome tooltip 1!',
   trigger: 'awesome tooltip1'
-};
-
-export const tooltip2 = {
-  hoverComponent: 'I am the awesome tooltip 2!',
-  trigger: 'awesome tooltip2'
-};  
+}; 
 
 export const tooltip1styles = {
   trigger: {
@@ -36,24 +31,5 @@ export const tooltip1styles = {
     background: '#cccccc',
     display: 'none',
     position: 'absolute'
-  }
-};
-
-export const tooltip2styles = {
-  trigger: {
-    background: 'rgb(58, 98, 247)',
-    width: '250px',
-    marginLeft: '5px'
-  },
-
-  hoverComponent: {
-    height: '25px',
-    overflowY: 'auto',
-    width: '250px',
-    borderRadius: '15px',
-    background: 'rgb(216, 226, 99)',
-    display: 'none',
-    position: 'absolute',
-    padding: '5px'
   }
 };
